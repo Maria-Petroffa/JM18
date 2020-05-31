2ce142751024ac7c7cb99436e8dfafb0a278df8f
Maria Petrova <Maria Petrova>
2020-05-31 17:53:43 +0300

    update 31/05

---
+++ src/components/inputTime.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Row, Col, Slider, InputNumber,
+} from 'antd';
+export default class InputTime extends React.Component {
+  handleChangeMin = (value) => {
+    const { onChangeMin } = this.props;
+    onChangeMin(value);
+  };
+
+  handleChangeSec = (value) => {
+    const { onChangeSec } = this.props;
+    onChangeSec(value);
+  };
+
+  handleChangeSlider = (value) => {
+    const { onChangeSlider } = this.props;
+    onChangeSlider(value);
+  };
+
+  render() {
+    const { value: { timeInputDisable, timePrev } } = this.props;
+    const sec = timePrev % 60;
+    const min = Math.trunc(timePrev / 60);
+
+    return (
+      <>
+        <Row>
+          <Col span={20}>
+            <Slider
+              min={0}
+              max={3600}
+              onChange={this.handleChangeSlider}
+              step={15}
+              value={typeof timePrev === 'number' ? timePrev : 0}
+            />
+          </Col>
+          <br />
+          <br />
+          <br />
+          <Col span={24}>
+            <InputNumber
+              min={0}
+              max={720}
+              step={1}
+              style={{ margin: '0 16px' }}
+              value={min}
+              onChange={this.handleChangeMin}
+              disabled={timeInputDisable}
+            />
+            <InputNumber
+              min={0}
+              max={60}
+              style={{ margin: '0 16px' }}
+              step={1}
+              value={sec}
+              onChange={this.handleChangeSec}
+              disabled={timeInputDisable}
+            />
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
+
+InputTime.propTypes = {
+  value: PropTypes.object,
+  onChangeSec: PropTypes.func,
+  onChangeMin: PropTypes.func,
+  onChangeSlider: PropTypes.func,
+};
+
+
+InputTime.defaultProps = {
+  value: { timeInputDisable: false, timePrev: 0 },
+  onChangeSec: null,
+  onChangeMin: null,
+  onChangeSlider: null,
+};
--- src/components/progressTime.js
+++ src/components/progressTime.js
@@ -1,36 +1,48 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { Progress } from 'antd';
-
+import sound from '../sound.mp3';
 export default class ProgressTime extends React.Component {
-    count = () => {
-      const { value } = this.props;
-      const { timeCount } = value;
-      const min = Math.trunc(timeCount / 60);
-      const sec = timeCount % 60;
-      return (<div>{`осталось ${min} : ${sec}`}</div>);
+  count = () => {
+    const { value: { timeCount } } = this.props;
+    const min = Math.trunc(timeCount / 60);
+    const sec = timeCount % 60;
+    return (<div>{`осталось ${min} : ${sec}`}</div>);
+  }
+
+  onSound = (percent) => {
+    if (percent < 100) {
+      return null;
     }
 
-    render() {
-      const { value: { timeStart, timeCount } } = this.props;
-      if (timeStart === 0) { return null; }
-      const progressPercent = 100 - Math.trunc((timeCount / timeStart) * 100);
-      return (
-        < >
-          {this.count()}
-          <br />
-          <Progress
-            type="circle"
-            strokeColor={{
-              '0%': '#108ee9',
-              '100%': '#87d068',
-            }}
-            percent={progressPercent}
-          />
-        </>
-      );
-    }
+    const autoplay = true;
+    return <audio src={sound} autoPlay={autoplay} type={"audio/mpeg"} />;
+  }
+
+  render() {
+    const { value: { timeStart, timeCount } } = this.props;
+    if (timeStart === 0) { return null; }
+    const progressPercent = 100 - Math.trunc((timeCount / timeStart) * 100);
+    return (
+      < >
+        {this.count()}
+        <br />
+        <Progress
+          type="circle"
+          strokeColor={{
+            '0%': '#108ee9',
+            '100%': '#87d068',
+          }}
+          percent={progressPercent}
+        />
+        {this.onSound(progressPercent)}
+      </>
+    );
+  }
 }
 ProgressTime.propTypes = {
-
+  value: PropTypes.object,
+};
+ProgressTime.defaultProps = {
+  value: { timeStart: 0, timeCount: 0 },
 };
